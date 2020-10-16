79f4e8a74a436fd592565986b08db4579ed324cf
Duran <danigduran@gmail.com>
2020-10-16 17:03:50 +0700

    4 section done

--- SQLzoo.sql
+++ SQLzoo.sql
@@ -408,3 +408,56 @@ SELECT name, continent
 FROM world x
 WHERE population > ALL(SELECT population*3 FROM world y WHERE x.continent = y.continent AND population > 0 AND y.name != x.name)
 
+-- 4th section SUM and COUNT
+
+-- Total world population
+-- 1.
+
+-- Show the total population of the world.
+
+-- world(name, continent, area, population, gdp)
+SELECT SUM(population)
+FROM world
+
+-- List of continents
+-- 2.
+
+-- List all the continents - just once each. 
+select distinct continent from world
+
+-- GDP of Africa
+-- 3.
+
+-- Give the total GDP of Africa 
+select sum(gdp) from world where continent like 'africa'
+
+-- Count the big countries
+-- 4.
+
+-- How many countries have an area of at least 1000000 
+select count(name) from world where area >= 1000000
+
+-- Baltic states population
+-- 5.
+
+-- What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+ select sum(population) from world where name in ('estonia', 'latvia', 'lithuania')
+
+--  Counting the countries of each continent
+-- 6.
+
+-- For each continent show the continent and number of countries.
+select continent, count(name) from world group by continent
+
+-- Counting big countries in each continent
+-- 7.
+
+-- For each continent show the continent and number of countries with populations of at least 10 million. 
+select continent, count(name) from world where population >= 10000000 group by continent
+
+-- Counting big continents
+-- 8.
+
+-- List the continents that have a total population of at least 100 million. 
+select continent from world group by continent having sum(population) >= 100000000
+
