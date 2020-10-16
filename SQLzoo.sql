7af15f36f676fd4b6bb5830d0b34ea51df5e1e6e
Duran <danigduran@gmail.com>
2020-10-16 18:33:11 +0700

    finish project

--- SQLzoo.sql
+++ SQLzoo.sql
@@ -461,3 +461,487 @@ select continent, count(name) from world where population >= 10000000 group by c
 -- List the continents that have a total population of at least 100 million. 
 select continent from world group by continent having sum(population) >= 100000000
 
+-- 5 section
+
+-- 1.
+
+-- The first example shows the goal scored by a player with the last name 'Bender'. The * says to list all the columns in the table - a shorter way of saying matchid, teamid, player, gtime
+
+-- Modify it to show the matchid and player name for all goals scored by Germany. To identify German players, check for: teamid = 'GER'
+SELECT matchid, player FROM goal 
+  WHERE teamid LIKE 'ger'
+
+--   2.
+
+-- From the previous query you can see that Lars Bender's scored a goal in game 1012. Now we want to know what teams were playing in that match.
+
+-- Notice in the that the column matchid in the goal table corresponds to the id column in the game table. We can look up information about game 1012 by finding that row in the game table.
+
+-- Show id, stadium, team1, team2 for just game 1012
+SELECT id,stadium,team1,team2
+  FROM game
+WHERE id = 1012
+
+-- 3.
+
+-- You can combine the two steps into a single query with a JOIN.
+
+-- SELECT *
+--   FROM game JOIN goal ON (id=matchid)
+
+-- The FROM clause says to merge data from the goal table with that from the game table. The ON says how to figure out which rows in game go with which rows in goal - the matchid from goal must match id from game. (If we wanted to be more clear/specific we could say
+-- ON (game.id=goal.matchid)
+
+-- The code below shows the player (from the goal) and stadium name (from the game table) for every goal scored.
+
+-- Modify it to show the player, teamid, stadium and mdate for every German goal.
+SELECT player, teamid, stadium, mdate
+FROM game
+JOIN goal ON (id=matchid AND teamid='GER')
+
+-- 4.
+
+-- Use the same JOIN as in the previous question.
+
+-- Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+SELECT team1, team2, player
+FROM game
+JOIN goal ON (id=matchid AND player LIKE 'mario%')
+
+-- 5.
+
+-- The table eteam gives details of every national team including the coach. You can JOIN goal to eteam using the phrase goal JOIN eteam on teamid=id
+
+-- Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10
+SELECT player, teamid, coach, gtime
+FROM goal
+  JOIN eteam ON (teamid=id AND gtime<=10)
+
+--   6.
+
+-- To JOIN game with eteam you could use either
+-- game JOIN eteam ON (team1=eteam.id) or game JOIN eteam ON (team2=eteam.id)
+
+-- Notice that because id is a column name in both game and eteam you must specify eteam.id instead of just id
+
+-- List the dates of the matches and the name of the team in which 'Fernando Santos' was the team1 coach.
+SELECT mdate, teamname
+FROM game
+  JOIN eteam ON (team1=eteam.id AND coach LIKE '%Santos')
+
+-- 7.
+
+-- List the player for every goal scored in a game where the stadium was 'National Stadium, Warsaw'
+
+SELECT player
+FROM goal
+  JOIN game ON (id=matchid AND stadium = 'National Stadium, Warsaw')
+
+-- 8.
+-- The example query shows all goals scored in the Germany-Greece quarterfinal.
+
+-- Instead show the name of all players who scored a goal against Germany.
+SELECT DISTINCT(player)
+FROM game
+  JOIN goal ON matchid = id
+WHERE ((team1='GER' OR team2='GER') AND teamid <> 'GER'
+
+-- 9.
+-- Show teamname and the total number of goals scored.
+-- COUNT and GROUP BY
+-- You should COUNT(*) in the SELECT line and GROUP BY teamname
+SELECT teamname, COUNT(player)
+FROM eteam
+  JOIN goal ON id=teamid
+GROUP BY teamname
+
+-- 10.
+-- Show the stadium and the number of goals scored in each stadium.
+SELECT stadium, COUNT(player) AS goals
+FROM game
+  JOIN goal ON (id=matchid)
+GROUP BY stadium
+
+-- 11.
+-- For every match involving 'POL', show the matchid, date and the number of goals scored.
+SELECT matchid, mdate, COUNT(player) AS goals
+FROM game
+  JOIN goal ON (matchid=id AND (team1 = 'POL' OR team2 = 'POL'))
+GROUP BY matchid, mdate
+
+-- 12.
+-- For every match where 'GER' scored, show matchid, match date and the number of goals scored by 'GER'
+SELECT id, mdate, COUNT(player)
+FROM game
+  JOIN goal ON (id=matchid AND (team1 = 'GER' OR team2 = 'GER') AND teamid='GER')
+GROUP BY id, mdate
+-- 13.
+-- List every match with the goals scored by each team as shown. This will use "CASE WHEN" which has not been explained in any previous exercises.
+-- mdate	team1	score1	team2	score2
+-- 1 July 2012	ESP	4	ITA 	0
+-- 10 June 2012	ESP	1	ITA	1
+-- 10 June 2012	IRL	1	CRO	3
+-- ...
+
+-- Notice in the query given every goal is listed. If it was a team1 goal then a 1 appears in score1, otherwise there is a 0. You could SUM this column to get a count of the goals scored by team1. Sort your result by mdate, matchid, team1 and team2.
+SELECT mdate,
+       team1,
+       SUM(CASE WHEN teamid = team1 THEN 1 ELSE 0 END) AS score1,
+       team2,
+       SUM(CASE WHEN teamid = team2 THEN 1 ELSE 0 END) AS score2 FROM
+    game LEFT JOIN goal ON (id = matchid)
+    GROUP BY mdate,team1,team2
+    ORDER BY mdate, matchid, team1, team2
+
+/*
+Seventh section of sqlzoo, more JOIN
+*/
+
+
+--#1
+/*
+List the films where the yr is 1962 [Show id, title]
+*/
+SELECT id, title
+FROM movie
+WHERE yr=1962
+
+--#2
+/*
+Give year of 'Citizen Kane'.
+*/
+SELECT yr
+FROM movie
+WHERE title = 'Citizen Kane'
+
+--#3
+/*
+List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+*/
+SELECT id, title, yr
+FROM movie
+WHERE title LIKE '%star trek%'
+ORDER BY yr
+
+--#4
+/*
+What are the titles of the films with id 11768, 11955, 21191
+*/
+SELECT title
+FROM movie
+WHERE id IN ( 11768, 11955, 21191)
+
+--#5
+/*
+What id number does the actor 'Glenn Close' have?
+*/
+SELECT id
+FROM actor
+WHERE name = 'Glenn Close'
+
+--#6
+/*
+What is the id of the film 'Casablanca'
+*/
+SELECT id
+FROM movie
+WHERE title = 'Casablanca'
+
+--#7
+/*
+Obtain the cast list for 'Casablanca'.
+what is a cast list?
+Use movieid=11768 this is the value that you obtained in the previous question.
+*/
+SELECT name
+FROM actor, casting
+WHERE id=actorid AND movieid = (SELECT id FROM movie WHERE title = 'Casablanca')
+
+--#8
+/*
+Obtain the cast list for the film 'Alien'
+*/
+SELECT name
+FROM actor
+  JOIN casting ON (id=actorid AND movieid = (SELECT id FROM movie WHERE title = 'Alien'))
+
+--#9
+/*
+List the films in which 'Harrison Ford' has appeared
+*/
+SELECT title
+FROM movie
+  JOIN casting ON (id=movieid AND actorid = (SELECT id FROM actor WHERE name = 'Harrison Ford'))
+
+--#10
+/*
+List the films where 'Harrison Ford' has appeared - but not in the star role.
+[Note: the ord field of casting gives the position of the actor. If ord=1 then this actor is in the starring role]
+*/
+SELECT title
+FROM movie
+    JOIN casting ON (id=movieid AND actorid = (SELECT id FROM actor WHERE name = 'Harrison Ford') AND ord != 1)
+
+--#11
+/*
+List the films together with the leading star for all 1962 films.
+*/
+SELECT title, name
+FROM movie JOIN casting ON (id=movieid)
+JOIN actor ON (actor.id = actorid)
+WHERE ord=1 AND  yr = 1962
+
+--#12
+/*
+Which were the busiest years for 'John Travolta', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
+*/
+SELECT yr,COUNT(title) FROM
+  movie JOIN casting ON movie.id=movieid
+         JOIN actor   ON actorid=actor.id
+WHERE name='John Travolta'
+GROUP BY yr
+HAVING COUNT(title)=(SELECT MAX(c) FROM
+(SELECT yr,COUNT(title) AS c FROM
+   movie JOIN casting ON movie.id=movieid
+         JOIN actor   ON actorid=actor.id
+ WHERE name='John Travolta'
+ GROUP BY yr) AS t)
+
+--#13
+/*
+List the film title and the leading actor for all of the films 'Julie Andrews' played in.
+*/
+SELECT title, name FROM movie
+JOIN casting x ON movie.id = movieid
+JOIN actor ON actor.id =actorid
+WHERE ord=1 AND movieid IN
+(SELECT movieid FROM casting y
+JOIN actor ON actor.id=actorid
+WHERE name='Julie Andrews')
+
+--#14
+/*
+Obtain a list in alphabetical order of actors who've had at least 30 starring roles.
+*/
+SELECT name
+FROM actor
+  JOIN casting ON (id = actorid AND (SELECT COUNT(ord) FROM casting WHERE actorid = actor.id AND ord=1)>=30)
+GROUP BY name
+
+--#15
+/*
+List the films released in the year 1978 ordered by the number of actors in the cast.
+*/
+SELECT title, COUNT(actorid) as cast
+FROM movie JOIN casting on id=movieid
+WHERE yr = 1978
+GROUP BY title
+ORDER BY cast DESC
+
+--#16
+/*
+List all the people who have worked with 'Art Garfunkel'.
+*/
+SELECT DISTINCT name
+FROM actor JOIN casting ON id=actorid
+WHERE movieid IN (SELECT movieid FROM casting JOIN actor ON (actorid=id AND name='Art Garfunkel')) AND name != 'Art Garfunkel'
+GROUP BY name
+
+/*
+Eighth section of sqlzoo, Using NULL
+*/
+
+--#1
+/*
+List the teachers who have NULL for their department.
+*/
+SELECT name
+FROM teacher
+WHERE dept IS NULL
+
+--#2
+/*
+Note the INNER JOIN misses the teacher with no department and the department with no teacher
+*/
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
+
+--#3
+/*
+Use a different JOIN so that all teachers are listed.
+*/
+SELECT teacher.name, dept.name
+FROM teacher LEFT JOIN dept
+          ON (teacher.dept=dept.id)
+
+--#4
+/*
+Use a different JOIN so that all departments are listed.
+*/
+SELECT teacher.name, dept.name
+FROM teacher RIGHT JOIN dept
+          ON (teacher.dept=dept.id)
+
+--#5
+/*
+Use COALESCE to print the mobile number. Use the number '07986 444 2266' there is no number given. Show teacher name and mobile number or '07986 444 2266'
+*/
+SELECT name,
+COALESCE(mobile, '07986 444 2266')
+FROM teacher
+
+--#6
+/*
+Use the COALESCE function and a LEFT JOIN to print the name and department name. Use the string 'None' where there is no department.
+*/
+SELECT COALESCE(teacher.name, 'NONE'), COALESCE(dept.name, 'None')
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id)
+
+--#7
+/*
+Use COUNT to show the number of teachers and the number of mobile phones.
+*/
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher
+
+--#8
+/*
+Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
+*/
+SELECT dept.name, COUNT(teacher.name)
+FROM teacher RIGHT JOIN dept ON (teacher.dept=dept.id)
+GROUP BY dept.name
+
+--#9
+/*
+Use CASE to show the name of each teacher followed by 'Sci' if the the teacher is in dept 1 or 2 and 'Art' otherwise.
+*/
+SELECT teacher.name,
+CASE WHEN dept.id = 1 THEN 'Sci'
+     WHEN dept.id = 2 THEN 'Sci'
+     ELSE 'Art' END
+FROM teacher LEFT JOIN dept ON (teacher.dept=dept.id)
+
+--#10
+/*
+Use CASE to show the name of each teacher followed by 'Sci' if the the teacher is in dept 1 or 2 show 'Art' if the dept is 3 and 'None' otherwise.
+*/
+SELECT teacher.name,
+CASE
+WHEN dept.id = 1 THEN 'Sci'
+WHEN dept.id = 2 THEN 'Sci'
+WHEN dept.id = 3 THEN 'Art'
+ELSE 'None' END
+FROM teacher LEFT JOIN dept ON (dept.id=teacher.dept)
+
+/*
+Final section of sqlzoo, Self JOIN
+*/
+
+
+--#1
+/*
+How many stops are in the database.
+*/
+SELECT COUNT(*)
+FROM stops
+
+--#2
+/*
+Find the id value for the stop 'Craiglockhart'
+*/
+SELECT id
+FROM stops
+WHERE name = 'Craiglockhart'
+
+--#3
+/*
+Give the id and the name for the stops on the '4' 'LRT' service.
+*/
+SELECT id, name
+FROM stops
+    JOIN route ON id=stop
+WHERE company = 'LRT' AND num=4
+
+--#4
+/*
+The query shown gives the number of routes that visit either London Road (149) or Craiglockhart (53). Run the query and notice the two services that link these stops have a count of 2.
+Add a HAVING clause to restrict the output to these two routes
+*/
+SELECT company, num, COUNT(*) AS visits
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING visits=2
+
+--#5
+/*
+Execute the self join shown and observe that b.stop gives all the places you can get to from Craiglockhart, without changing routes. Change the query so that it shows the services from Craiglockhart to London Road.
+*/
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop=149
+
+--#6
+/*
+The query shown is similar to the previous one, however by joining two copies of the stops table we can refer to stops by name rather than by number. Change the query so that the services between 'Craiglockhart' and 'London Road' are shown.
+If you are tired of these places try 'Fairmilehead' against 'Tollcross'
+*/
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart'AND stopb.name = 'London Road'
+
+--#7
+/*
+Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+*/
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company =b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Haymarket' AND stopb.name='Leith'
+
+--#8
+/*
+Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
+*/
+SELECT DISTINCT a.company, a.num
+FROM route a
+  JOIN route b ON (a.num=b.num AND a.company=b.company)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross'
+
+--#9
+/*
+Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself. Include the company and bus no. of the relevant services.
+*/
+SELECT stopa.name, a.company, a.num
+FROM route a
+  JOIN route b ON (a.num=b.num AND a.company=b.company)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopb.name = 'Craiglockhart'
+
+--#10
+/*
+Find the routes involving two buses that can go from Craiglockhart to Sighthill.
+Show the bus no. and company for the first bus, the name of the stop for the transfer,
+and the bus no. and company for the second bus.
+*/
+SELECT DISTINCT a.num, a.company, stopb.name ,  c.num,  c.company
+FROM route a JOIN route b
+ON (a.company = b.company AND a.num = b.num)
+JOIN ( route c JOIN route d ON (c.company = d.company AND c.num= d.num))
+JOIN stops stopa ON (a.stop = stopa.id)
+JOIN stops stopb ON (b.stop = stopb.id)
+JOIN stops stopc ON (c.stop = stopc.id)
+JOIN stops stopd ON (d.stop = stopd.id)
+WHERE  stopa.name = 'Craiglockhart' AND stopd.name = 'Sighthill'
+            AND  stopb.name = stopc.name
+ORDER BY LENGTH(a.num), b.num, stopb.id, LENGTH(c.num), d.num
